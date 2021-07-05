7a53920c206bdef1b6608386b62b70d57b41a742
Maxwellcaieira <81589030+Maxwellcaieira@users.noreply.github.com>
2021-07-05 16:46:45 -0300

    Add files via upload

---
+++ tcc/registro/cadastrar.php
@@ -0,0 +1,225 @@
+<link rel="icon" href="../img/icon.ico" type="image/x-icon" />
+<?php
+session_start();
+ob_start();
+$btnCadUsuario = filter_input(INPUT_POST, 'btnCadUsuario', FILTER_SANITIZE_STRING);
+if($btnCadUsuario){
+	include_once 'conexao.php';
+	$dados_rc = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+	
+	$erro = false;
+	
+	$dados_st = array_map('strip_tags', $dados_rc);
+	$dados = array_map('trim', $dados_st);
+	
+	if(in_array('',$dados)){
+		$erro = true;
+		$_SESSION['msg'] = "<div class='alert alert-danger'>Necessário preencher todos os campos!</div>";
+	}elseif((strlen($dados['senha'])) < 6){
+		$erro = true;
+		$_SESSION['msg'] = "<div class='alert alert-danger'>A senha deve ter no minímo 6 caracteres!</div>";
+	}elseif(stristr($dados['senha'], "'")) {
+		$erro = true;
+		$_SESSION['msg'] = "<div class='alert alert-danger'>Caracter ( ' ) utilizado na senha é inválido!</div>";
+	}else{
+		$result_usuario = "SELECT id FROM usuarios WHERE usuario='". $dados['usuario'] ."'";
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)){
+			$erro = true;
+			$_SESSION['msg'] = "<div class='alert alert-danger'>Este CPF já está cadastrado!</div>";
+		}
+		
+		$result_usuario = "SELECT id FROM usuarios WHERE email='". $dados['email'] ."'";
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)){
+			$erro = true;
+			$_SESSION['msg'] = "<div class='alert alert-danger'>Este e-mail já está cadastrado!</div>";
+		}
+	}
+	
+	
+	//var_dump($dados);
+	if(!$erro){
+		//var_dump($dados);
+		$dados['senha'] = password_hash($dados['senha'], PASSWORD_DEFAULT);
+		
+		$result_usuario = "INSERT INTO usuarios (nome, email, usuario, senha) VALUES (
+						'" .$dados['nome']. "',
+						'" .$dados['email']. "',
+						'" .$dados['usuario']. "',
+						'" .$dados['senha']. "'
+						)";
+		$resultado_usario = mysqli_query($conn, $result_usuario);
+		if(mysqli_insert_id($conn)){
+			$_SESSION['msgcad'] = "<div class='alert alert-success'>Usuário cadastrado com sucesso!</div>";
+			header("Location: login.php");
+		}else{
+			$_SESSION['msg'] = "<div class='alert alert-danger'>Erro ao cadastrar o usuário!</div>";
+		}
+	}
+	
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<!DOCTYPE html>
+<html lang="pt-br">
+<link rel="icon" href="icon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="css/styler.css" type="text/css" />
+<link href="https://fonts.googleapis.com/css?family=Hepta+Slab:400,700|Lato:400,700&displey=swp" rel="stylesheet"/>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+<body bgcolor="black"></body>
+<A href="../index.html">
+<IMG width="150" SRC = "img/logo2.png" TITLE="D'egustar Whisky" ALIGN = "left"  HSPACE="100" >
+</A>
+<BR>
+<style>
+#search-box {
+    position: relative;
+    width: 70%;
+    margin: 0;
+}
+#search-form {
+    height: 40px;
+    border: 1px solid #999;
+    -webkit-border-radius: 5px;
+    -moz-border-radius: 5px;
+    border-radius: 0px;
+    background-color: #fff;
+    overflow: hidden;
+}
+#search-text {
+    font-size: 14px;
+    color: #1B6DB0;
+    border-width: 0;
+    background: transparent;
+}
+#search-box input[type="text"] {
+    width: 90%;
+    padding: 11px 0 12px 1em;
+    color: #1B6DB0;
+    outline: none;
+}
+#search-button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 42px;
+    width: 80px;
+    font-size: 14px;
+    color: #fff;
+    text-align: center;
+    line-height: 42px;
+    border-width: 0;
+    background-color: #1B6DB0; /*--define a cor do botão Buscar--*/
+    -webkit-border-radius: 0px 5px 5px 0px;
+    -moz-border-radius: 0px 5px 5px 0px;
+    border-radius: 0px 5px 5px 0px;
+    cursor: pointer;
+}
+#search-button2 {
+    position: absolute;
+    top: 0;
+    right: -200px;
+    height: 42px;
+    width: 80px;
+    font-size: 14px;
+    color: #fff;
+    text-align: center;
+    line-height: 42px;
+    border-width: 0;
+    background-color: #1B6DB0; 
+    -webkit-border-radius: 0px 0px 0px 0px;
+    -moz-border-radius: 0px 0px 0px 0px;
+    border-radius: 0px 0px 0px 0px;
+    cursor: pointer;
+}
+</style>
+
+
+
+<br><br>
+<style>
+    body{
+        margin: 0;
+    }
+    
+    #menu ul{
+        margin: 0;
+        list-style: none;
+        text-align: center;
+    }
+
+    #menu ul li{
+        display: inline;
+
+    }
+
+    #menu ul li a{
+        padding: 10px 10px;
+        display: inline-block;
+        color: cornsilk;
+        text-decoration: none;
+    }
+
+    #menu ul li a:hover{
+        color: darkgreen;
+    }
+</style>
+
+</head>
+<br><br><br>
+<body>
+<div id = "menu">
+    <ul>
+        <li><a href="../index.html"><FONT COLOR = "#FFFFFF"><FONT SIZE = "4"></FONT>Início</FONT></FONT></a></li>
+        <li><a href="../catalogo.html"><FONT COLOR = "#FFFFFF"><FONT SIZE = "4"></FONT>Catálogo</FONT></FONT></a></li>
+        <li><a href="../quem somos.html"><FONT COLOR = "#FFFFFF"><FONT SIZE = "4"></FONT>Quem Somos</FONT></FONT></a></li>
+        <li><a href="../fale conosco.html"><FONT COLOR = "#FFFFFF"><FONT SIZE = "4"></FONT>Fale Conosco</FONT></FONT></a></li>
+    </ul>
+</div>
+<br><br>
+
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Cadastrar</title>
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/signin.css" rel="stylesheet">
+	</head>
+	<body>
+		<div class="container">
+			<div class="form-signin" style="background: #42dea4;">
+				<h2>Cadastrar Cliente</h2>
+				<?php
+					if(isset($_SESSION['msg'])){
+						echo $_SESSION['msg'];
+						unset($_SESSION['msg']);
+					}
+				?>
+				<form method="POST" action="">
+					<!--<label>Nome</label>-->
+					<input type="text" name="nome" placeholder="Digite o nome e o sobrenome" class="form-control"><br>
+					
+					<!--<label>E-mail</label>-->
+					<input type="text" name="email" placeholder="Digite o seu e-mail" class="form-control"><br>
+					
+					<!--<label>Usuário</label>-->
+					<input type="text" name="usuario" placeholder="Digite seu CPF" class="form-control"><br>
+					
+					<!--<label>Senha</label>-->
+					<input type="password" name="senha" placeholder="Digite a senha" class="form-control"><br>
+					
+					<input type="submit" name="btnCadUsuario" value="Cadastrar" class="btn btn-success"><br><br>
+					
+					<div class="row text-center" style="margin-top: 20px;"> 
+						Lembrou? <a href="login.php">Clique aqui</a> para logar
+					</div>
+				</form>
+			</div>
+		</div>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
